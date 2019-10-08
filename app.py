298d4139fe3de7d9e52e9da2819d46651591fc2d
wattanai sathuphan <wattanai.sat@ku.th>
2019-10-08 10:10:16 +0700

    flask user part

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import firebase_api
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def root():
+    return "Working"
+
+@app.route('/adduser', methods = ['POST'])
+def adduser():
+    # try:
+        if request.method == 'POST':
+            # uid = request.args.get("uid")
+            # username = request.args.get("uname")
+            # print("got args")
+            data = request.json
+            firebase_api.add_new_user(data["uid"], data["name"])
+            return jsonify({"status": "ok",
+                            "uid": data["uid"]})
+    # except:
+        # return jsonify({"status": "error"})
+
+@app.route('/userdata', methods = ['GET'])
+def getUserData():
+    try:
+        if request.method == 'GET':
+            uid = request.args.get("uid")
+            return jsonify({"status": "ok",
+                            "uid": uid,
+                            "userData": firebase_api.get_user_data(uid)})
+    except:
+        return jsonify({"status": "error"})
+
+if __name__ == "__main__":
+    app.run(debug = False,host="0.0.0.0", port=5000)
