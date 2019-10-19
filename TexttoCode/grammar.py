1440231d16939bd597525433f3a8dffba27296a5
lastthanya <iamjui2010@hotmail.com>
2019-10-20 00:13:23 +0700

    'mergecarefulword'

--- TexttoCode/grammar.py
+++ TexttoCode/grammar.py
@@ -1,22 +1,24 @@
 import deepcut
 import dialogflow_api
 import detection
-'''
-def greater(lis):
-    ans=[]
-    i=0
-    while i<len(lis):
-        if lis[i]=='มาก' and lis[i+1]=='กว่า':
-            ans.append('')
-            ans[i]=ans[i].replace('','มากกว่า')
-            i+=2
-        else:
-            ans.append(lis[i])
-            i+=1
-'''
+CAREFUL_WORD = [['มาก', 'กว่า'], ['น้อย', 'กว่า']]
+def mergecarefulword(lis):
+    for j in range (len(CAREFUL_WORD)):
+        i=0
+        ans=[]
+        while i<len(lis):
+            if lis[i]==CAREFUL_WORD[j][0] and lis[i+1]==CAREFUL_WORD[j][1]:
+                ans.append('')
+                ans[i]=ans[i].replace('',CAREFUL_WORD[j][0]+CAREFUL_WORD[j][1])
+                i+=2
+            else:
+                ans.append(lis[i])
+                i+=1
+        lis=ans
+    return ans
+
 def grammar(s):
     x = deepcut.tokenize(s)
-    #x=grammar(s)
     ans=[]
     stringans=''
     check=False
@@ -30,14 +32,9 @@ def grammar(s):
         else:
             check=True
             ans.append(i)
-    #greater(ans)
-    #lesser(ans)
-   # equal(ans)
-
-    print(ans)
+    ans = mergecarefulword(ans)
     ans.append(stringans)
     ans = detection.tran(ans,0)
     return ans
 
-#print(grammar('สวัสดีครับมากกว่าข้อความมากกว่านี้มาจากจุ้ยเอง'))
-#dialogflow_api.detect_intent_texts(i)
+#
